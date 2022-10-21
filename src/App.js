29138d68bedc695c03200d57d63c544b79eb85ba
omar ashraf <omarashrafothman1234@gmail.com>
2022-10-21 08:20:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/header";
+import Balance from "./components/balance";
+import Expense from "./components/incomExpences";
+import TransactionList from "./components/transactionList";
+import AddTransaction from "./components/addTransaction";
+import { GlobalProvider } from "./context/glopalState";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <Expense />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
